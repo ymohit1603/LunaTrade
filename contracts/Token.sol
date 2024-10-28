370b30f7def6bea7e96615a9e18f9078199f7c9e
ymohit1603 <mohityadav0330@gmail.com>
2024-10-28 20:51:01 +0530

    Token transfer delegation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -10,8 +10,15 @@ contract Token {
 
     //Track Balances
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
 
     //Send Tokens
 
@@ -32,13 +39,43 @@ contract Token {
     ) public returns (bool success) {
         //Require that sender has enough tokens to spend
         require(balanceOf[msg.sender] >= _value);
+
+        _transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function _transfer(address _from, address _to, uint256 _value) internal {
         require(_to != address(0));
 
-        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+        balanceOf[_from] = balanceOf[_from] - _value;
         balanceOf[_to] = balanceOf[_to] + _value;
 
-        //emit event
-        emit Transfer(msg.sender, _to, _value);
+        emit Transfer(_from, _to, _value);
+    }
+
+    function approve(
+        address _spender,
+        uint _value
+    ) public returns (bool success) {
+        require(_spender != address(0));
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        //check approval
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+        //spend tokens
+        _transfer(_from, _to, _value);
 
         return true;
     }
