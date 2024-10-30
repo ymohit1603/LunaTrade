f55eac1f7ce2122678f7bcec7d2cd4fc48d94081
ymohit1603 <mohityadav0330@gmail.com>
2024-10-30 14:32:24 +0530

    add: make order

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -8,6 +8,8 @@ contract Exchange {
     address public feeAccount;
     uint256 public feePercent;
     mapping(address => mapping(address => uint256)) public tokens;
+    mapping(uint256 => _Order) public orders;
+    uint256 public orderCount;
 
     event Deposit(address token, address user, uint256 amount, uint256 balance);
     event Withdraw(
@@ -16,6 +18,27 @@ contract Exchange {
         uint256 amount,
         uint256 balance
     );
+
+    event Order(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGIve,
+        uint256 timestamp
+    );
+
+    struct _Order {
+        //Attributes of an order
+        uint256 id;
+        address user;
+        address tokenGet;
+        uint256 amountGet;
+        address tokenGive;
+        uint256 amountGive;
+        uint256 timestamp;
+    }
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
@@ -50,5 +73,35 @@ contract Exchange {
         address _user
     ) public view returns (uint256) {
         return tokens[_token][_user];
+    }
+
+    //Make and cancel orders
+
+    function makeOrder(
+        address _tokenGet,
+        uint256 _amountGet,
+        address _tokenGive,
+        uint256 _amountGive
+    ) public {
+        orderCount += 1;
+        orders[orderCount] = _Order(
+            orderCount,
+            msg.sender,
+            _tokenGet,
+            _amountGet,
+            _tokenGive,
+            _amountGive,
+            block.timestamp
+        );
+
+        emit Order(
+            orderCount,
+            msg.sender,
+            _tokenGet,
+            _amountGet,
+            _tokenGive,
+            _amountGive,
+            block.timestamp
+        );
     }
 }
